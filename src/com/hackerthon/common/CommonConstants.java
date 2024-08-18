6e68780d4031eb29e5b81eccf6329f6670b5d6e2
Thusala Piyarisi <thusalapi@gmail.com>
2024-08-18 15:06:22 +0530

    chore: Add CommonConstants class for XPath parameters

---
+++ src/com/hackerthon/common/CommonConstants.java
@@ -0,0 +1,29 @@
+package com.hackerthon.common;
+
+/**
+ * Class containing CommonConstants for XPath parameters.
+ */
+public class CommonConstants {
+    public static final String XPATH_EMPLOYEE_ID_KEY = "XpathEmployeeIDKey";
+    public static final String XPATH_EMPLOYEE_NAME_KEY = "XpathEmployeeNameKey";
+    public static final String XPATH_EMPLOYEE_ADDRESS_KEY = "XpathEmployeeAddressKey";
+    public static final String XPATH_FACULTY_NAME_KEY = "XpathFacultyNameKey";
+    public static final String XPATH_DEPARTMENT_KEY = "XpathDepartmentKey";
+    public static final String XPATH_DESIGNATION_KEY = "XpathDesignationKey";
+    public static final String EMPLOYEE_QUERY_PATH = "employeeQueryPath";
+    public static final String CONFIG_FILE_PATH = "/config/config.properties";
+
+    public static final String CONFIGURE_PROPERTIES_PATH = "/configure.properties";
+    public static final String XML_SOURCE_PATH = "xmlSourcePath";
+    public static final String XSLT_SOURCE_PATH = "xsltSourcePath";
+    public static final String OUTPUT_RESULT_PATH = "outputResultPath";
+    public static final String RESPONSE_XML_PATH = "responseXmlPath";
+
+    public static final String EMPLOYEE_ID = "employeeId";
+    public static final String EMPLOYEE_NAME = "employeeName";
+    public static final String EMPLOYEE_ADDRESS = "employeeAddress";
+    public static final String FACULTY_NAME = "facultyName";
+    public static final String DEPARTMENT = "department";
+    public static final String DESIGNATION = "designation";
+
+}
